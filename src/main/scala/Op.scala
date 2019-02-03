614077b76e6b182570cf4a10ea55cb6dfd9f3abc
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-02-03 17:39:36 +0100

    renaming: map -> contramap

--- src/main/scala/Op.scala
+++ src/main/scala/Op.scala
@@ -2,5 +2,5 @@
   * Created by mtumilowicz on 2019-01-28.
   */
 trait Op[R, A] extends (A => R) {
-  def map[B](f: B => A): Op[R, B] = this.compose(f).apply
+  def contramap[B](f: B => A): Op[R, B] = this.compose(f).apply
 }
--- src/test/scala/OpTest.scala
+++ src/test/scala/OpTest.scala
@@ -7,16 +7,16 @@ class OpTest extends FunSuite with Matchers {
   test("size of the collection basing on the list") {
     val sizer: Op[Int, List[String]] = _.size
 
-    sizer.map((set: Set[String]) => set.toList).apply(Set()) should be(0)
-    sizer.map((set: Set[String]) => set.toList).apply(Set("a")) should be(1)
-    sizer.map((set: Set[String]) => set.toList).apply(Set("a", "b", "c")) should be(3)
+    sizer.contramap((set: Set[String]) => set.toList).apply(Set()) should be(0)
+    sizer.contramap((set: Set[String]) => set.toList).apply(Set("a")) should be(1)
+    sizer.contramap((set: Set[String]) => set.toList).apply(Set("a", "b", "c")) should be(3)
   }
 
   test("predicate: isEven") {
     val isEven: Op[Boolean, Int] = _ % 2 == 0
 
-    isEven.map((s: String) => s.toInt).apply("2") should be(true)
-    isEven.map((s: String) => s.toInt).apply("3") should be(false)
+    isEven.contramap((s: String) => s.toInt).apply("2") should be(true)
+    isEven.contramap((s: String) => s.toInt).apply("3") should be(false)
   }
 
 }
